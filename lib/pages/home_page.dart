bfdd0ea00920881aae9760cf862b9c36d4be23f6
Martin Villa <martin.villa0703@gmail.com>
2023-11-10 22:40:13 -0500

    creacion de page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _nameState();
+}
+
+class _nameState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
