1df14f221edf5e87381954352298585d75020574
Martin Villa <martin.villa0703@gmail.com>
2023-11-11 09:55:00 -0500

    Textfield con atributos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,17 +18,48 @@ class _nameState extends State<HomePage> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
-                TextField(),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Ingrese el titulo",
+                    label: Text("Nombre"),
+                    focusColor: Colors.amber,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(
+                        color: Colors.amber,
+                        width: 5,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 5,
+                      ),
+                    ),
+                    icon: Icon(Icons.people),
+                    suffix: Icon(Icons.person_2_outlined),
+                    filled: true,
+                    fillColor: Colors.indigoAccent,
+                    hoverColor: Colors.redAccent,
+                    prefixIcon:
+                        IconButton(onPressed: () {}, icon: Icon(Icons.abc)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(color: Colors.red, width: 10),
+                    ),
+                  ),
+                ),
+                TextField(
+                  //enabled: false,
+                  decoration: InputDecoration(
+                    hintText: "Ingrese Dirección",
+                    label: Text("Dirección"),
+                    icon: Icon(Icons.directions),
+                    filled: true,
+                    fillColor: Colors.indigoAccent,
+                  ),
+                ),
               ],
             ),
           ),
